79043d4a559a5681f51ecfb413f4ea2d24f60343
RAJESH <gole8701@gmail.com>
2020-07-18 00:55:52 +0530

    Updates

---
+++ precache-manifest.4c8b432e7fb521946fd25d6cd002a7ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63c1115a99afe0d4ac26628a8c9f53b6",
+    "url": "/totoreact/index.html"
+  },
+  {
+    "revision": "4dbc457f49eceef9d41c",
+    "url": "/totoreact/static/css/main.2f0e452d.chunk.css"
+  },
+  {
+    "revision": "c97c55990a5b064e5c22",
+    "url": "/totoreact/static/js/2.a5f63a23.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/totoreact/static/js/2.a5f63a23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4dbc457f49eceef9d41c",
+    "url": "/totoreact/static/js/main.cd578664.chunk.js"
+  },
+  {
+    "revision": "48aa2ba4546963241db0",
+    "url": "/totoreact/static/js/runtime-main.835202ed.js"
+  }
+]);
